03fa6166666165d11658306c51c12b4b0a07ba17
Adam Ponting <adam.ponting@gmail.com>
2020-05-07 13:40:17 +0100

    completed 2.11

--- modern-javascript/chapter-2/sandbox.js
+++ modern-javascript/chapter-2/sandbox.js
@@ -1,9 +1,38 @@
-let email = 'mario@thenetninja.co.uk';
-console.log(email);
+// numbers
 
-// let result = email.lastIndexOf('n');
-// let result = email.slice(0, 5);
-// let result = email.substr(2, 5);
-// let result = email.replace('m', 'w');
-let result = email.replace('n', 'w');
+// let radius = 10;
+// const pi = 3.14;
+
+// console.log(radius, pi);
+
+// math operators - +, -, *, /, **, %
+
+// console.log(10 / 2);
+// let result = radius % 3;
+// let result = pi * radius ** 2;
+
+// order of operation - B I D M A S
+
+// let result = 5 * (10 - 3) ** 2;
+// console.log(result);
+
+// shorthands
+let likes = 10;
+
+// likes = likes + 1;
+// likes++;
+
+// likes = likes + 10;
+// likes += 10;
+
+// likes *= 10;
+// likes /= 2;
+// console.log(likes);
+
+// NaN - not a number
+
+// console.log(5 / 'hello');
+// console.log(5 * 'hello');
+
+let result = 'the blog has ' + likes + ' likes.';
 console.log(result);
