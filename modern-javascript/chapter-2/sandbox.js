adf734f11f48bf446dbff2dfe6eedfad41724b8a
Adam Ponting <adam.ponting@gmail.com>
2020-05-07 15:04:52 +0100

    completed 2.14

--- modern-javascript/chapter-2/sandbox.js
+++ modern-javascript/chapter-2/sandbox.js
@@ -1,25 +1,4 @@
-// arrays
+// null & undefined
+let age = null;
 
-let ninjas = ['shaun', 'ryu', 'chun-li'];
-// console.log(ninjas);
-
-// ninjas[1] = 'ken';
-// console.log(ninjas[1]);
-
-// let ages = [20, 25, 30, 35];
-// console.log(ages[2]);
-
-// let random = ['shaun', 'crystal', 30, 20];
-// console.log(random);
-// array length
-// console.log(ninjas.length);
-
-// array methods
-
-// let result = ninjas.join(', ');
-// let result = ninjas.indexOf('chun-li');
-// let result = ninjas.concat(['ken', 'crystal']);
-let result = ninjas.unshift('ken');
-result = ninjas.shift();
-
-console.log(result, ninjas);
+console.log(age, age * 3, `the age is ${age}`);
