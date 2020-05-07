c7113ea304089e1436eb4a156f3c2444afdb592d
Adam Ponting <adam.ponting@gmail.com>
2020-05-07 15:37:03 +0100

    completed 2.17

--- modern-javascript/chapter-2/sandbox.js
+++ modern-javascript/chapter-2/sandbox.js
@@ -1,14 +1,15 @@
-let age = 25;
-// loose comparison (different types can still be equal)
+// type conversion
+// let score = '100';
 
-// console.log(age == 25);
-// console.log(age == '25');
-// console.log(age != 25);
-// console.log(age != '25');
+// score = Number(score);
+// console.log(score + 1);
+// console.log(typeof score);
 
-// strict comparison (different types cannot be equal)
+// let result = Number('hello');
+// let result = String(50);
+// let result = Boolean(100);
+// let result = Boolean(0);
+// let result = Boolean('0');
+// let result = Boolean('');
 
-console.log(age === 25);
-console.log(age === '25');
-console.log(age !== 25);
-console.log(age !== '25');
+// console.log(result, typeof result);
