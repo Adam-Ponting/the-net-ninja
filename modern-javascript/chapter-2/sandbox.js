aeedbd449cfb53ae2007397d754f7c349d20e9bc
Adam Ponting <adam.ponting@gmail.com>
2020-05-07 12:56:29 +0100

    completed 2.1

---
+++ modern-javascript/chapter-2/sandbox.js
@@ -0,0 +1,14 @@
+let age = 25;
+let year = 2019;
+
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+// points = 200;
+
+var score = 75;
+console.log(score);
