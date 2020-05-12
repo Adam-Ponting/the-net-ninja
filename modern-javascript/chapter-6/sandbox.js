906585789458bbd3f21bee31d7876306da506110
Adam Ponting <adam.ponting@gmail.com>
2020-05-12 15:24:17 +0100

    done 6.46

---
+++ modern-javascript/chapter-6/sandbox.js
@@ -0,0 +1,13 @@
+// const para = document.querySelector('p');
+// const para = document.querySelector('.error');
+// const para = document.querySelector('div.error');
+
+// console.log(para);
+
+// // query multiple elements at once
+// const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
+console.log(errors);
+// console.log(Array.from(paras));
+// paras.forEach((p) => console.log(p));
+// console.log(paras[1], errors[0]);
