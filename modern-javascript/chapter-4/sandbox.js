71b174c1d6b7ff1d7152290331d9cca4a9a1fa8e
Adam Ponting <adam.ponting@gmail.com>
2020-05-12 09:06:16 +0100

    completed 4.34

--- modern-javascript/chapter-4/sandbox.js
+++ modern-javascript/chapter-4/sandbox.js
@@ -1,50 +1,13 @@
-// arrow functions
+const name = 'shaun';
 
-// regular function
-// const calcArea = function (radius) {
-//   return 3.14 * radius ** 2;
-// };
+// function
 
-// arrow function
-const calcArea = (radius) => 3.14 * radius ** 2;
+const greet = () => 'hello';
 
-const area = calcArea(5);
-console.log('area is:', area);
+let resultOne = greet();
+console.log(resultOne);
 
-// practise arrow functions
+// method
 
-// const greet = function(){
-//   return 'hello, world';
-// }
-
-const greet = () => 'hello world';
-greet();
-console.log(greet());
-
-// const bill = function(products, tax){
-//   let total = 0;
-//   for(let i = 0; i < products.length; i++){
-//     total += products[i] + products[i] * tax;
-//   }
-//   return total;
-// }
-
-const bill = (products, tax) => {
-  let total = 0;
-  for (let i = 0; i < products.length; i++) {
-    total += products[i] + products[i] * tax;
-  }
-  return total;
-};
-console.log(bill([10, 15, 30], 0.2));
-
-// const bill = (products, tax) => {
-//   let total = 0;
-//   for (let i = 0; i < products.length; i++) {
-//     total += products[i] + products[i] * tax;
-//   }
-//   return total;
-// };
-
-// console.log(greet());
-// console.log(bill([10, 15, 30], 0.2));
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
