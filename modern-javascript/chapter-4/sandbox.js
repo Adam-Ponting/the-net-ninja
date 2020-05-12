57f5f8d41320e28db6e6ca5a6b49d01d48b465db
Adam Ponting <adam.ponting@gmail.com>
2020-05-12 09:02:10 +0100

    completed 4.33

--- modern-javascript/chapter-4/sandbox.js
+++ modern-javascript/chapter-4/sandbox.js
@@ -1,12 +1,50 @@
-// return statements
+// arrow functions
 
-// const speak = function (name, time) {
-//   console.log(`good ${time}, ${name}!`);
+// regular function
+// const calcArea = function (radius) {
+//   return 3.14 * radius ** 2;
 // };
 
-const calcArea = function (radius) {
-  return 3.14 * radius ** 2;
-};
+// arrow function
+const calcArea = (radius) => 3.14 * radius ** 2;
 
 const area = calcArea(5);
 console.log('area is:', area);
+
+// practise arrow functions
+
+// const greet = function(){
+//   return 'hello, world';
+// }
+
+const greet = () => 'hello world';
+greet();
+console.log(greet());
+
+// const bill = function(products, tax){
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// }
+
+const bill = (products, tax) => {
+  let total = 0;
+  for (let i = 0; i < products.length; i++) {
+    total += products[i] + products[i] * tax;
+  }
+  return total;
+};
+console.log(bill([10, 15, 30], 0.2));
+
+// const bill = (products, tax) => {
+//   let total = 0;
+//   for (let i = 0; i < products.length; i++) {
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// };
+
+// console.log(greet());
+// console.log(bill([10, 15, 30], 0.2));
