01a9946da4fa7d1c81fb052dc71b4f6e4bd35755
Adam Ponting <adam.ponting@gmail.com>
2020-06-01 16:53:50 +0100

    done 6

--- object-oriented-js/objects.js
+++ object-oriented-js/objects.js
@@ -3,10 +3,16 @@ class User {
     this.email = email;
     this.name = name;
   }
+  login() {
+    console.log(this.email + ' just logged in');
+  }
+  logout() {
+    console.log(this.email, 'just logged out');
+  }
 }
 
 const userOne = new User('ryu@ninjas.com', 'Ryu');
 const userTwo = new User('mario@ninjas.com', 'Mario');
 
-console.log(userOne);
-console.log(userTwo);
+userOne.login();
+userTwo.logout();
