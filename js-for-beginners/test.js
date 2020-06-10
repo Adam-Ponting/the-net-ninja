b83b5041559b3f0df1734feea6893a06033103ee
Adam Ponting <adam.ponting@gmail.com>
2020-06-10 14:15:55 +0100

    done 18

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,5 +1,10 @@
-const links = document.querySelectorAll('a');
-for (let i = 0; i < links.length; i++) {
-  console.log('this is link number ' + i, links[i]);
+for (let index = 0; index < 10; index++) {
+  if (index === 5 || index === 3) {
+    continue;
+  }
+  console.log(index);
+  if (index === 7) {
+    break;
+  }
 }
-console.log('All links now looped');
+console.log('broken out of the loop');
