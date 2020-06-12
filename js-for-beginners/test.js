bd67ec98cdb3e77fd90b307a3ce58c70ed2fb84c
Adam Ponting <adam.ponting@gmail.com>
2020-06-12 13:21:43 +0100

    done 23

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,1 +1,10 @@
-console.log(Math.max(7, 4, 8, 9, 1));
+var a = a;
+var b = 5;
+
+if (isNaN(a)) {
+  console.log('Tha is not a number');
+} else {
+  console.log('The meaning of like is ' + a * b);
+}
+
+console.log(a * b);
