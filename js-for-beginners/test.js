01d543c2c28a9f8b604b2b89d8aefd6b8692a921
Adam Ponting <adam.ponting@gmail.com>
2020-06-12 11:35:59 +0100

    done 20

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,6 +1,8 @@
-const links = document.querySelectorAll('a');
-// console.log(links);
-// console.log(Array.from(links));
-for (let i = 0; i < links.length; i++) {
-  console.log(links[i]);
+function getAverage(a, b, c, d, e, f) {
+  var average = (a + b + c + d + e + f) / 6;
+  console.log(average);
+  return average;
 }
+
+var myResult = getAverage(7, 8, 9, 10, 11, 12);
+console.log('The average is ' + myResult);
