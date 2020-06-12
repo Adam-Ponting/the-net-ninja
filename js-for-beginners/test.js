a77666eb8343c8d7ae63eaad8cfbcbab5da94f96
Adam Ponting <adam.ponting@gmail.com>
2020-06-12 12:59:54 +0100

    done 22

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,12 +1,1 @@
-function getAverage(a, b) {
-  var average = (a + b) / 2;
-  console.log(average);
-  return average;
-}
-
-var myResult = getAverage(7, 11);
-
-function logResult() {
-  console.log('The average is ' + myResult + ' inside the function');
-}
-logResult();
+console.log(Math.max(7, 4, 8, 9, 1));
