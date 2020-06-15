c9f9beabf8ee615d878f1b66896b8d06755ce0d6
Adam Ponting <adam.ponting@gmail.com>
2020-06-15 16:48:15 +0100

    done 25

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,3 +1,10 @@
-var string1 = 'a';
-var string2 = 'B';
-console.log(string1 < string2);
+// var str = 'Hello, World!';
+// var str2 = str.slice(2, 9);
+// // console.log(str2);
+// // console.log(str);
+// var str3 = str.slice(-2);
+// console.log(str3);
+var tags = 'meat, ham, salami, pork, beef, chicken';
+// console.log(tags);
+var tagsArray = tags.replace(/,/g, '').split(' ');
+console.log(tagsArray);
