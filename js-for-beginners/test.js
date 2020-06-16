5002b685600f31096ff8a56e29a5c0d62a5953a4
Adam Ponting <adam.ponting@gmail.com>
2020-06-16 09:03:13 +0100

    done 26

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,10 +1,15 @@
-// var str = 'Hello, World!';
-// var str2 = str.slice(2, 9);
-// // console.log(str2);
-// // console.log(str);
-// var str3 = str.slice(-2);
-// console.log(str3);
-var tags = 'meat, ham, salami, pork, beef, chicken';
-// console.log(tags);
-var tagsArray = tags.replace(/,/g, '').split(' ');
-console.log(tagsArray);
+// var myArray = [];
+// myArray[0] = 25;
+// myArray[1] = 35;
+// myArray[2] = true;
+// myArray[3] = 'hello';
+// myArray[2] = false;
+// console.log(myArray);
+
+const myArray2 = [100, 10, 20, 23, 22, 30, 40, 'hi', false, 'false', true];
+// // console.log(myArray2);
+// var myArray3 = new Array(5);
+// console.log(myArray3.length);
+// myArray3.push(1, 2, 3, 4, 5);
+// console.log(myArray3.length);
+console.log(myArray2.sort((a, b) => a - b));
