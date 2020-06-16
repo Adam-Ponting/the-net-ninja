a0317603fc99b6abd3f2469a7f2a16f15bab6e4f
Adam Ponting <adam.ponting@gmail.com>
2020-06-16 09:23:21 +0100

    done 28

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,6 +1,19 @@
-var myString = new String();
-myString = 'heLLO';
-console.log(myString.toLowerCase());
+// var myCar = new Object();
+// myCar.maxSpeed = 50;
+// myCar.driver = 'Shaun';
+// // console.log(myCar);
+// myCar.drive = function () {
+//   console.log('Now driving!');
+// };
 
-var myString2 = 'Hi there';
-console.log(myString2.toUpperCase());
+// myCar.drive();
+
+var myCar2 = {
+  maxSpeed: 100,
+  driver: 'Adam',
+  drive: function (speed, time) {
+    console.log(speed * time);
+  },
+};
+// console.log(myCar2.maxSpeed);
+console.log(myCar2.drive(50, 3));
