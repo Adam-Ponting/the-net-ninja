61e39453141f33995cbd67e763ec44f92085798c
Adam Ponting <adam.ponting@gmail.com>
2020-06-16 09:13:32 +0100

    done 27

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,15 +1,6 @@
-// var myArray = [];
-// myArray[0] = 25;
-// myArray[1] = 35;
-// myArray[2] = true;
-// myArray[3] = 'hello';
-// myArray[2] = false;
-// console.log(myArray);
+var myString = new String();
+myString = 'heLLO';
+console.log(myString.toLowerCase());
 
-const myArray2 = [100, 10, 20, 23, 22, 30, 40, 'hi', false, 'false', true];
-// // console.log(myArray2);
-// var myArray3 = new Array(5);
-// console.log(myArray3.length);
-// myArray3.push(1, 2, 3, 4, 5);
-// console.log(myArray3.length);
-console.log(myArray2.sort((a, b) => a - b));
+var myString2 = 'Hi there';
+console.log(myString2.toUpperCase());
