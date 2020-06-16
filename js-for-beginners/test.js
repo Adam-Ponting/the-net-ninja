ff5f9dafc5044a077da2c644d318d2c6f0ed54c6
Adam Ponting <adam.ponting@gmail.com>
2020-06-16 09:29:09 +0100

    done 29

--- js-for-beginners/test.js
+++ js-for-beginners/test.js
@@ -1,19 +1,12 @@
-// var myCar = new Object();
-// myCar.maxSpeed = 50;
-// myCar.driver = 'Shaun';
-// // console.log(myCar);
-// myCar.drive = function () {
-//   console.log('Now driving!');
-// };
-
-// myCar.drive();
-
 var myCar2 = {
   maxSpeed: 100,
   driver: 'Adam',
   drive: function (speed, time) {
     console.log(speed * time);
   },
+  logDriver: function () {
+    console.log('Driver name is: ' + this.driver);
+  },
 };
-// console.log(myCar2.maxSpeed);
-console.log(myCar2.drive(50, 3));
+// console.log(myCar2.drive(50, 3));
+myCar2.logDriver();
