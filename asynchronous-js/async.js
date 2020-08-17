7c94a4b059bd2dafddcb235fd0bbca6a88f8de6c
Adam Ponting <adam.ponting@gmail.com>
2020-08-17 16:25:55 +0100

    done 3

--- asynchronous-js/async.js
+++ asynchronous-js/async.js
@@ -1,19 +1,36 @@
 /* eslint-env jquery */
 
 window.onload = function () {
-    // native js
-    // var http = new XMLHttpRequest();
-    // http.onreadystatechange = function () {
-    //     if (http.readyState === 4 && http.status === 200) {
-    //         console.log(JSON.parse(http.response));
-    //     }
-    // };
-    // http.open('GET', 'data/tweets.json', true);
-    // http.send();
-    // console.log('test');
-    // jquery method
-    $.get('data/tweets.json', function (data) {
-        console.log(data);
+    function handleError(jqXHR, textStatus, error) {
+        console.log(error);
+    }
+
+    $.ajax({
+        type: 'GET',
+        url: 'data/tweets.json',
+        success: cbTweets,
+        error: handleError,
     });
-    console.log('test');
+
+    function cbTweets(data) {
+        console.log(data);
+        $.ajax({
+            type: 'GET',
+            url: 'data/friends.json',
+            success: cbFriends,
+            error: handleError,
+        });
+    }
+
+    function cbFriends(data) {
+        console.log(data);
+        $.ajax({
+            type: 'GET',
+            url: 'data/videos.json',
+            success: function (data) {
+                console.log(data);
+            },
+            error: handleError,
+        });
+    }
 };
