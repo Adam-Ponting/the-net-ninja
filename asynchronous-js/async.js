39d7eb8468beaf714e3d645d828f66e0160720ec
Adam Ponting <adam.ponting@gmail.com>
2020-09-05 10:44:26 +0100

    done 5

--- asynchronous-js/async.js
+++ asynchronous-js/async.js
@@ -1,18 +1,49 @@
 /* eslint-env jquery */
 
 window.onload = function () {
-    $.get('data/tweets.json')
-        .then(function (tweets) {
-            console.log(tweets);
-            return $.get('data/friends.json');
-        })
-        .then(function (friends) {
-            console.log(friends);
-            return $.get('data/videos.json');
-        })
-        .then(function (videos) {
-            console.log(videos);
-        });
+    function genWrap(generator) {
+        var gen = generator();
+
+        function handle(yielded) {
+            if (!yielded.done) {
+                yielded.value.then(function (data) {
+                    return handle(gen.next(data));
+                });
+            }
+        }
+        return handle(gen.next());
+    }
+
+    genWrap(function* () {
+        var tweets = yield $.get('data/tweets.json');
+        console.log(tweets);
+        var friends = yield $.get('data/friends.json');
+        console.log(friends);
+        var videos = yield $.get('data/videos.json');
+        console.log(videos);
+    });
+
+    // function* gen() {
+    //     var x = yield 10;
+    //     console.log(x);
+    // }
+
+    // var myGen = gen();
+    // console.log(myGen.next());
+    // console.log(myGen.next(50));
+
+    // $.get('data/tweets.json')
+    //     .then(function (tweets) {
+    //         console.log(tweets);
+    //         return $.get('data/friends.json');
+    //     })
+    //     .then(function (friends) {
+    //         console.log(friends);
+    //         return $.get('data/videos.json');
+    //     })
+    //     .then(function (videos) {
+    //         console.log(videos);
+    //     });
     // function get(url) {
     //     return new Promise(function (resolve, reject) {
     //         var xhttp = new XMLHttpRequest();
