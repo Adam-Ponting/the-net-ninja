e090862c6d9273242b93c54359c1dea731774064
Adam Ponting <adam.ponting@gmail.com>
2020-09-14 11:44:02 +0100

    done 8

--- js-dom-tutorial/app.js
+++ js-dom-tutorial/app.js
@@ -1,5 +1,8 @@
 const bookList = document.querySelector('#book-list');
-// console.log(bookList.parentNode);
-// console.log(bookList.parentElement);
-// console.log(bookList.childNodes);
-console.log(bookList.children);
+
+// console.log(bookList.nextElementSibling);
+console.log(bookList.nextElementSibling);
+console.log(bookList.previousElementSibling);
+console.log(bookList.previousSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br /> Too Cool for everyone else';
