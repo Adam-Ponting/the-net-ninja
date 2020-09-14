690c59afcf717ef5247adaf2d7faa1f5ffe794cd
Adam Ponting <adam.ponting@gmail.com>
2020-09-14 12:02:43 +0100

    done 9

--- js-dom-tutorial/app.js
+++ js-dom-tutorial/app.js
@@ -1,8 +1,17 @@
-const bookList = document.querySelector('#book-list');
+const buttons = document.querySelectorAll('#book-list .delete');
 
-// console.log(bookList.nextElementSibling);
-console.log(bookList.nextElementSibling);
-console.log(bookList.previousElementSibling);
-console.log(bookList.previousSibling);
+console.log(buttons);
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const li = e.target.parentElement;
+        console.log(li);
+        console.log(li.parentNode);
+        li.parentNode.removeChild(li);
+    });
+});
 
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br /> Too Cool for everyone else';
+const link = document.querySelector('#page-banner a');
+link.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(`Navigation to ${e.target.textContent} was prevented`);
+});
