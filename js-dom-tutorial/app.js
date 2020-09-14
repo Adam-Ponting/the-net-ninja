203c786855d3fe99ba61046d0a2050c204d460fe
Adam Ponting <adam.ponting@gmail.com>
2020-09-14 15:51:23 +0100

    done 17

--- js-dom-tutorial/app.js
+++ js-dom-tutorial/app.js
@@ -51,10 +51,25 @@ searchBar.addEventListener('keyup', (e) => {
     books.forEach((book) => {
         const title = book.firstElementChild.textContent;
         if (title.toLowerCase().indexOf(term) !== -1) {
-            console.log(title.toLowerCase().indexOf(term), title);
             book.style.display = 'block';
         } else {
             book.style.display = 'none';
         }
     });
+});
+
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+
+tabs.addEventListener('click', (e) => {
+    if (e.target.tagName === 'LI') {
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach((panel) => {
+            if (panel === targetPanel) {
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active');
+            }
+        });
+    }
 });
