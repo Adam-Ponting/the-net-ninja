62599f56af490f6e4480b490e075432779634d87
Adam Ponting <adam.ponting@gmail.com>
2020-09-14 09:58:34 +0100

    done 5

--- js-dom-tutorial/app.js
+++ js-dom-tutorial/app.js
@@ -1,7 +1,10 @@
-const wrap = document.querySelector('#book-list li:nth-child(2) .name');
-
-// console.log(wrap);
-
 var books = document.querySelectorAll('#book-list li .name');
+console.log(books);
+// Array.from(books).forEach((book) => console.log(book));
+books.forEach((book, i) => {
+    book.textContent += ` (book title) Test: ${i}`;
+});
 
-Array.from(books).forEach((el) => console.log(el));
+const bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
